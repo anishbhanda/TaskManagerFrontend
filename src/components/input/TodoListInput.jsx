871720fd41e707994949b8208eb548f978e5678f
anishbhandari <anish@insightfultechno.com>
2025-08-22 23:52:53 +0530

    todoList

--- src/components/input/TodoListInput.jsx
+++ src/components/input/TodoListInput.jsx
@@ -21,32 +21,38 @@ const TodoListInput = ({ todoList, setTodoList }) => {
     return (
         <div>
             {todoList.map((item, index) => {
-                <div key={item} className="">
-                    <p className="">
-                        <span className="">
+                <div
+                    key={item}
+                    className="flex justify-between bg-gray-50 border border-gray-100 px-3 py-2 rounded-md mb-3 mt-2 "
+                >
+                    <p className="text-xs text-black">
+                        <span className="text-xs text-gray-400 font-semibold mr-2">
                             {index < 9 ? `0${index + 1}` : index + 1}
                         </span>
                         {item}
                     </p>
 
                     <button
-                        className=""
+                        className="cursor-pointer"
                         onClick={() => handleDeleteOption(index)}
                     >
                         <HiOutlineTrash className="" />
                     </button>
                 </div>;
             })}
-            <div className="">
+            <div className="flex items-center gap-5 mt-4">
                 <input
                     type="text"
                     placeholder="Enter Task"
                     value={option}
                     onChange={({ target }) => setOption(target.value)}
-                    className=""
+                    className="w-full text-[13px] text-black outline-none bg-white border border-gray-100 px-3 py-2 rounded-md"
                 />
-                <button className="" onClick={handleAddOption}>
-                    <HiMiniPlus className="" /> Add
+                <button
+                    className="card-btn text-nowrap"
+                    onClick={handleAddOption}
+                >
+                    <HiMiniPlus className="text-lg cursor-pointer" /> Add
                 </button>
             </div>
         </div>
--- src/pages/Admin/CreateTask.jsx
+++ src/pages/Admin/CreateTask.jsx
@@ -9,6 +9,7 @@ import moment from "moment";
 import { LuTrash2 } from "react-icons/lu";
 import SelectDropDown from "../../components/input/SelectDropDown";
 import SelectUsers from "../../components/input/SelectUsers";
+import AddAttachmentsInput from "../../components/input/AddAttachmentsInput";
 
 const CreateTask = () => {
     const location = useLocation();
@@ -49,11 +50,64 @@ const CreateTask = () => {
     };
 
     // Create Task
-    const createTask = () => {};
+    const createTask = async () => {
+        setLoading(true);
+
+        try {
+            const todolist = taskData.todoChecklist?.map((item) => ({
+                text: item,
+                completed: false,
+            }));
+
+            const response = await axiosInstance.post(
+                API_PATHS.TASKS.CREATE_TASK,
+                {
+                    ...taskData,
+                    dueDate: new Data(taskData.dueDate).toISOString(),
+                    todoChecklist: todolist,
+                }
+            );
+            toast.success("Task Created Successfully");
+            clearData();
+        } catch (error) {
+            console.error("Error Creating Task:", error);
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+    };
     // Update Task
     const updateTask = async () => {};
 
-    const handleSubmit = async () => {};
+    const handleSubmit = async () => {
+        setError(null);
+
+        if (!taskData.title.trim()) {
+            setError("Title is required");
+            return;
+        }
+
+        if (!taskData.description.trim()) {
+            setError("Due date is required.");
+            return;
+        }
+
+        if (taskData.assignedTo?.length === 0) {
+            setError("Task is not assigned to any member");
+            return;
+        }
+
+        if (taskData.todoChecklist?.length === 0) {
+            setError("Add atleast one todo task");
+            return;
+        }
+
+        if (taskId) {
+            updateTask();
+            return;
+        }
+        createTask();
+    };
 
     //get Task info by ID
     const getTasksDetailsByID = async () => {};
@@ -160,7 +214,9 @@ const CreateTask = () => {
                                 </div>
 
                                 <div className="mt-3">
-                                    <label className="text-xs font-medium text-slate-600">TODO Checklist</label>
+                                    <label className="text-xs font-medium text-slate-600">
+                                        TODO Checklist
+                                    </label>
                                     <TodoListInput
                                         todoList={taskData?.todoChecklist}
                                         setTodoList={(value) => {
@@ -170,6 +226,35 @@ const CreateTask = () => {
                                             );
                                         }}
                                     />
+                                </div>
+                                <div className="mt-3">
+                                    <label className="text-xs font-medium text-slate-600">
+                                        Add Attachments
+                                    </label>
+                                    <AddAttachmentsInput
+                                        attachments={taskData?.attachments}
+                                        setAttachments={(value) =>
+                                            handleValueChange(
+                                                "attachments",
+                                                value
+                                            )
+                                        }
+                                    />
+                                </div>
+
+                                {error && (
+                                    <p className="text-xs font-medium text-red-500 mt-5">
+                                        {error}
+                                    </p>
+                                )}
+                                <div className="flex justify-end mt-7">
+                                    <button
+                                        className="add-btn"
+                                        onClick={handleSubmit}
+                                        disabled={loading}
+                                    >
+                                        {taskId ? "UPDATE TASK" : "CREATE TASK"}
+                                    </button>
                                 </div>
                             </div>
                         </div>
